d5576d882e9dc97a4f3fe6bc182d27ecdc8f1bf5
prabhattiwarii <prabhattiwari9629@gmail.com>
2023-08-13 11:36:32 +0530

    chatroom done

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -1,11 +1,95 @@
-import React from 'react'
-import {user} from '../Joinn'
+import React, { useEffect, useState } from 'react';
+import { user } from '../Joinn';
+import socketIo from "socket.io-client";
+import sendImg from '../../images/msg.png';
+import crossImg from '../../images/cros.png';
+import './Chat.css';
+import Message from '../Message/Message';
+import ReactScrollToBottom from 'react-scroll-to-bottom'
+import InputEmoji from 'react-input-emoji'
+
+let socket;
+
+const ENDPOINT = "http://localhost:4500/";
+
 const Chat = () => {
-  return (
-    <div>
-        <h1>{user}</h1>
-    </div>
-  )
+    const [id, setId] = useState(""); 
+    const [messages, setMessages] = useState([])
+    const [newMessage, setNewMessage] = useState("")
+
+    const send = () => {
+        if (newMessage.trim() !== "") {
+            socket.emit('message', { message: newMessage, id }); 
+            setNewMessage(""); 
+        }
+    }
+    
+    useEffect(() => {
+        socket = socketIo(ENDPOINT, { transports: ['websocket'] });
+
+        socket.on('connect', () => {
+            console.log("Connected");
+            setId(socket.id); 
+            socket.emit('joined', { user });
+        });
+
+        socket.on('welcome', (data) => {
+            setMessages(prevMessages => [...prevMessages, data]);
+            console.log(data.user, data.message);
+        });
+
+        socket.on('userJoined', (data) => {
+            setMessages(prevMessages => [...prevMessages, data]);
+            console.log(data.user, data.message);
+        });
+
+        socket.on('leave', (data) => {
+            setMessages(prevMessages => [...prevMessages, data]);
+            console.log(data.user, data.message);
+        });
+
+        return () => {
+            socket.off();
+        };
+    }, []);
+
+    useEffect(() => {
+        socket.on('sendMessage', (data) => {
+            setMessages(prevMessages => [...prevMessages, data]);
+            console.log(data.user, data.message, data.id);
+        })
+        return () => {
+            socket.off();
+        }
+    }, [messages])
+
+    const handleChange = (newMessage) => {
+        setNewMessage(newMessage);
+    }
+
+    const handleChatClose = () => {
+        window.location.href = '/';
+    }
+
+    return (
+        <div className='chatPage'>
+            <div className="chatContainer">
+                <div className="header">
+                    <h2>Chat-Room</h2>
+                    <img src={crossImg} alt="close" onClick={handleChatClose}/>
+                </div>
+                <ReactScrollToBottom className="chatBox">
+                    {messages.map((item, i) => (
+                        <Message key={i} user={item.id === id ? '' : item.user} message={item.message} classs={item.id === id ? 'right' : 'left'} />
+                    ))}
+                </ReactScrollToBottom>
+                <div className="inputBox">
+                    <InputEmoji value={newMessage} onChange={handleChange} onEnter={send}/>
+                    <button onClick={send} id='sendBtn'><img src={sendImg} alt="" disabled={newMessage.trim() === ""}/></button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Chat
+export default Chat;
--- src/component/Joinn.js
+++ src/component/Joinn.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import './Joiin.css'
-import logo from '../images/logo.png'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Joiin.css';
+import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
 
+let user = "";
 
-let user;
+const sendUser = () => {
+    user = document.getElementById("JoinInput").value;
+    document.getElementById("JoinInput").value = "";
+};
+
 const Joinn = () => {
-    const sendUser = ()=>{
-        user = document.getElementById("JoinInput").value;
-        document.getElementById("JoinInput").value ="";
-    }
-  return (
-    <div className='JoinPage'>
-        <div className="JoinContainer">
-        <img src={logo} alt="img" />
-            <h1>Chat-Room</h1>
-            <input type="text" id='JoinInput' placeholder='Enter Your Name'/>
-            <Link to="/chat-room"><button className='loginbtn' onClick={sendUser}>Login</button></Link>
+    const [name, setName] = useState("");
+
+    return (
+        <div className='JoinPage'>
+            <div className="JoinContainer">
+                <img src={logo} alt="img" />
+                <h1>Chat-Room</h1>
+                <input type="text" id='JoinInput' placeholder='Enter Your Name' onChange={(e) => setName(e.target.value)} />
+                <Link onClick={(e) => !name ? e.preventDefault() : null} to="/chat-room"><button className='loginbtn' onClick={sendUser}>Login</button></Link>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
+
 export default Joinn;
-export {user}
+export { user };
